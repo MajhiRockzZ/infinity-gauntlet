1c56460680c3a305f4f35753f9d8c06fe97b461d
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2019-10-11 18:14:42 +0530

    feat: 😻 Able to evaluate a expression

--- src/evaluate.js
+++ src/evaluate.js
@@ -1,6 +1,19 @@
 const { environment } = require('./standard-library');
 const last = collection => collection[collection.length - 1];
 
-const evaluate = () => {};
+const apply = node => {
+    const fn = environment[node.name];
+    const args = node.arguments.map(evaluate);
+    if (typeof fn !== 'function') {
+        throw new TypeError(`${node.name} is not a functiion`);
+    }
+
+    return fn(...args);
+}
+
+const evaluate = (node) => {
+    if(node.type === 'CallExpression') return apply(node);
+    if(node.value) return node.value;
+};
 
 module.exports = { evaluate };
